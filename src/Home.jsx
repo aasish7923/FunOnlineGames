5efea03393f247321eabef45793320a7752bcc11
Aasish <accumgja2@gmail.com>
2026-06-01 20:04:54 +0545

    feat: update Home component to use Link for navigation and add Vercel configuration

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 
 import GameBox from "./GameBox";
-import NumberGame from "./NumberGame";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="flex flex-col  gap-5 min-h-screen bg-gray-100 p-5">
         <div className="grid grid-cols-4 gap-10 bg-gray-100 max-md:grid-cols-2 max-sm:grid-cols-1">
-            <a href="/number-game" className="h-auto"><GameBox image="Number-Game.png" title="Number Game" /></a>
-            <a href="/simon-says" className="h-auto"><GameBox image="SimonSays.png" title="Simon Says Game" /></a>
+          <Link to="/number-game" className="h-auto"><GameBox image="Number-Game.png" title="Number Game" /></Link>
+          <Link to="/simon-says" className="h-auto"><GameBox image="SimonSays.png" title="Simon Says Game" /></Link>
             {/* <a href="/number-game" className="h-auto"><GameBox image="Number-Game.png" title="Number Game" /></a> */}
         </div>
     </div>
